64cf9946b9868334525e43edca2510ae71203557
unknown <lpk.yolandafashion123@gmail.com>
2025-10-25 09:50:08 +0700

    Fix vercel.json for Flutter Web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6e5411625b0a5d785a75e74a4d3b06fb
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "73b359624702e5728300d4903b6c65c6",
+"flutter_bootstrap.js": "722fc03a81bf76b93b26d9635bd8b8b1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
